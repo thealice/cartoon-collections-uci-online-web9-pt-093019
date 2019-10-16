6c55661421918ba746c81761c67c7ddfea6bbabd
Alice Freda <alicefreda@gmail.com>
2019-10-16 22:54:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,14 +12,14 @@ def long_planeteer_calls(calls)
   calls.any? {|i| i.length > 4}
 end
 
-def find_the_cheese(array)
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  i = 0
-  while i < cheese_types.length
-    return cheese_types[i] if array.include?(cheese_types[i])
-    i += 1
-  # if array.include? { |i| cheese_types.include?(i) }
-
+  snacks.detect do |snack| 
+    cheese_types.include?(snack)
   end
+  # i = 0
+  # while i < cheese_types.length
+  #   return cheese_types[i] if array.include?(cheese_types[i])
+  #   i += 1
+  # end
 end
